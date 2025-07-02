29ce2c0b4a7db0182f3994436c1630dad12ad119
Eyve9ykecua7y <20243rd049@utez.edu.mx>
2025-07-02 01:36:25 -0600

    solo agregue el evento para que llamara la funcion de mostrar ventana

--- Menu/src/main/java/org/example/menu/HelloController.java
+++ Menu/src/main/java/org/example/menu/HelloController.java
@@ -2,13 +2,14 @@ package org.example.menu;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void handleDivision(ActionEvent event) {
+        DivisionVentana.show();
     }
 }
